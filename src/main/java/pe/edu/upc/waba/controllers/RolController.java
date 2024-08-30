c49972d60564e39ebbcfe87cdff873d7203f73d4
Briggitte HS <u201922070@upc.edu.pe>
2024-08-29 19:49:50 -0500

    clase rol

---
+++ src/main/java/pe/edu/upc/waba/controllers/RolController.java
@@ -0,0 +1,52 @@
+package pe.edu.upc.waba.controllers;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import pe.edu.upc.waba.dtos.RolDTO;
+import pe.edu.upc.waba.entities.Rol;
+import pe.edu.upc.waba.serviceinterfaces.IRolService;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/roles")
+public class RolController {
+    @Autowired
+    private IRolService rS;
+
+    @GetMapping
+    public List<RolDTO> list(){
+        return rS.list().stream().map(y->{
+            ModelMapper m= new ModelMapper();
+            return m.map(y, RolDTO.class);
+        }).collect(Collectors.toList());
+    }
+
+    @PostMapping
+    public void Insertar(@RequestBody RolDTO dto){
+        ModelMapper m = new ModelMapper();
+        Rol r = m.map(dto, Rol.class);
+        rS.insert(r);
+    }
+
+    @GetMapping("/{id}")
+    public RolDTO ListarId(@PathVariable("id") Integer id){
+        ModelMapper m= new ModelMapper();
+        RolDTO dto=m.map(rS.listarId(id), RolDTO.class);
+        return dto;
+    }
+
+    @PutMapping
+    public void modificar(@RequestBody RolDTO dto){
+        ModelMapper m = new ModelMapper();
+        Rol r = m.map(dto, Rol.class);
+        rS.update(r);
+    }
+
+    @DeleteMapping("/{id}")
+    public void eliminar(@PathVariable("id") Integer id){
+        rS.delete(id);
+    }
+}
